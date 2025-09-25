8b9b67e2c8f1591e13911c856aed943a92da4b2d
Viraj8464 <virajpatel8464@gmail.com>
2025-09-25 15:51:13 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,19 +1,11 @@
-# Generate a random suffix to make bucket name globally unique
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-# Create S3 bucket with unique name
 resource "aws_s3_bucket" "this" {
-  bucket        = "upload-bucket-data-pipeline-${random_id.suffix.hex}"
+  bucket = "myfirstbucket-8464"   # put quotes + make it unique
   force_destroy = true
 }
 
-# Upload sample object into the bucket
 resource "aws_s3_object" "sample_data" {
   bucket       = aws_s3_bucket.this.id
   key          = "data/sample.json"
   source       = "${path.module}/sample.json"
   content_type = "application/json"
 }
-
