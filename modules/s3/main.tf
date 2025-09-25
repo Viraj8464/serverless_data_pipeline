9e6aea9e3408b4130ed8ebce6885b90c5ce093ea
Viraj8464 <virajpatel8464@gmail.com>
2025-09-25 16:16:07 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,11 +1,14 @@
+# Create S3 bucket with fixed name
 resource "aws_s3_bucket" "this" {
-  bucket = "myfirstbucket-8464"   # put quotes + make it unique
+  bucket        = "myfirstbucket-8464"  # your fixed bucket name
   force_destroy = true
 }
 
+# Upload a sample object into the bucket
 resource "aws_s3_object" "sample_data" {
   bucket       = aws_s3_bucket.this.id
   key          = "data/sample.json"
   source       = "${path.module}/sample.json"
   content_type = "application/json"
 }
+
