4ec3f8cfab8834ee1184b03c227b3574a229491f
Viraj8464 <virajpatel8464@gmail.com>
2025-09-25 16:16:31 +0530

    Update output.tf

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,3 +1,9 @@
-output "myfirstbucket-8464" {
-  value = aws_s3_bucket.primary.bucket
+output "s3_bucket_name" {
+  description = "The name of the created S3 bucket"
+  value       = aws_s3_bucket.this.bucket
+}
+
+output "sample_object_url" {
+  description = "The S3 URL of the uploaded sample object"
+  value       = "s3://${aws_s3_bucket.this.bucket}/${aws_s3_object.sample_data.key}"
 }
