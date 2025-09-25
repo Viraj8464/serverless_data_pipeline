6b5acb9d16da4d3c643f986319b6c05957389311
Viraj8464 <virajpatel8464@gmail.com>
2025-09-25 15:36:28 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "this" {
-  bucket = myfirstbucket-8464
-
+  bucket = "myfirstbucket-8464-viraj"   # put quotes + make it unique
   force_destroy = true
 }
 
 resource "aws_s3_object" "sample_data" {
-  bucket = aws_s3_bucket.this.id
-  key    = "data/sample.json"
-  source = "${path.module}/sample.json"
+  bucket       = aws_s3_bucket.this.id
+  key          = "data/sample.json"
+  source       = "${path.module}/sample.json"
   content_type = "application/json"
 }
+
