70af452bb31baac44dc408205b6f681456f99307
Viraj8464 <virajpatel8464@gmail.com>
2025-09-28 15:20:41 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,15 +1,17 @@
+# Get your AWS account ID
 data "aws_caller_identity" "current" {}
 
+# Generate a random suffix (8 hex characters)
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
+# Create an S3 bucket with a unique name
 resource "aws_s3_bucket" "this" {
-  bucket = "upload-bucket-data-pipeline-234-${data.aws_caller_identity.current.account_id}-${random_id.suffix.hex}"
+  bucket = "upload-bucket-data-pipeline-${data.aws_caller_identity.current.account_id}-${random_id.suffix.hex}"
 
   tags = {
-    Name        = "upload-bucket-data-pipeline-234"
+    Name        = "upload-bucket-data-pipeline"
     Environment = "dev"
   }
 }
-
