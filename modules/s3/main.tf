f1fcb01e5dca2e820bf5b3da393485b1d531ac40
Viraj8464 <virajpatel8464@gmail.com>
2025-09-28 15:43:08 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,37 +1,13 @@
-# modules/s3/main.tf
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
 
-# Create S3 Bucket
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket        = "upload-bucket-data-pipeline-${random_id.bucket_suffix.hex}"
   force_destroy = true
 
-  # Optional: enable versioning
-  versioning {
-    enabled = true
-  }
-
   tags = {
-    Name = var.bucket_name
-    Environment = "dev"
-  }
-}
-
-# Optional: sample data upload
-resource "aws_s3_object" "sample_data" {
-  bucket  = aws_s3_bucket.this.bucket
-  key     = "data/sample.json"
-  source  = "modules/s3/sample.json"
-  content_type = "application/json"
-}
-
-# S3 Bucket Notification to Lambda
-resource "aws_s3_bucket_notification" "trigger" {
-  bucket = aws_s3_bucket.this.bucket
-
-  lambda_function {
-    events              = ["s3:ObjectCreated:*"]
-    lambda_function_arn = var.lambda_function_arn
-    filter_prefix       = "data/"
-    filter_suffix       = ".json"
+    Name        = "Data Pipeline Bucket"
+    Environment = "Dev"
   }
 }
