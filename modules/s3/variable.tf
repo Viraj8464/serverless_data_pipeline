07e23d026440e7215e098c2c2bed4055f5ce6d0a
Viraj8464 <virajpatel8464@gmail.com>
2025-09-28 15:33:43 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,4 +1,9 @@
 variable "bucket_name" {
-  description = "S3 Bucket Name"
+  description = "my-new-unique-bucket-viraj"
+  type        = string
+}
+
+variable "lambda_function_arn" {
+  description = "The ARN of the Lambda function to trigger on S3 events"
   type        = string
 }
