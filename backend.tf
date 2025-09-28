dce1240cbe445b87a6fbb2a178a6e1520e331b3a
Viraj8464 <virajpatel8464@gmail.com>
2025-09-28 16:31:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "upload-bucket-data-pipeline-8464"
-    key    = "serverless-pipeline/dev/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "eu-north-1"
   }
 }
