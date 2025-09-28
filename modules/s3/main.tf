7a9a00de006ed1babf8796b9dfac42d3bd84f0b9
Viraj8464 <virajpatel8464@gmail.com>
2025-09-28 16:29:10 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,13 +1,7 @@
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
+resource "aws_s3_bucket" "primary" {
+  bucket = "upload-bucket-data-pipeline-8464"
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket        = "upload-bucket-data-pipeline-8464-${random_id.bucket_suffix.hex}"
-  force_destroy = true
-
-  tags = {
-    Name        = "Data Pipeline Bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "secondary" {
+  bucket = "myfirstbucket-8464"
 }
