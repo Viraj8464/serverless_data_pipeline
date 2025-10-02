a74d0fc8878b387fc5b9d73a00f93890603e1942
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:46:08 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -7,3 +7,8 @@ variable "lambda_function_arn" {
   type        = string
   description = "The ARN of the Lambda function"
 }
+
+variable "lambda_function_name" {
+  type        = string
+  description = "The name of the Lambda function"
+}
