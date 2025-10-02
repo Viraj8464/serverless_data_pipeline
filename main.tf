275f9dd790c1ef1d19b9e9f44dfcdf55a34558c3
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:35:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,10 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "./modules/s3"
-  bucket_name = "upload-bucket-data-pipeline-8464"
+  source               = "./modules/s3"
+  bucket_name          = "upload-bucket-data-pipeline-8464"
+  lambda_function_arn  = module.lambda_function.arn
+  lambda_function_name = module.lambda_function.function_name
 }
 
 module "dynamodb_table" {
