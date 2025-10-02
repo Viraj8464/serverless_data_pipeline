6b14a240af59387d538cfd36ade8e8c7db174ed5
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:40:51 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -8,23 +8,11 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-# S3 → Lambda notification
 resource "aws_s3_bucket_notification" "lambda_trigger" {
   bucket = aws_s3_bucket.this.id
 
   lambda_function {
     lambda_function_arn = var.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"] # triggers on object creation
+    events              = ["s3:ObjectCreated:*"]
   }
-
-  depends_on = [aws_lambda_permission.allow_s3]
-}
-
-# Permission so S3 can invoke Lambda
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.this.arn
 }
