11160758c7e7b7beb197c3729411f5541931df1c
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:23:57 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-  acl    = "private"
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  count  = var.lambda_function_arn == null ? 0 : 1
+  bucket = aws_s3_bucket.this.id
 
-  tags = {
-    Name        = var.bucket_name
-    Environment = "Dev"
+  lambda_function {
+    lambda_function_arn = var.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
   }
 }
