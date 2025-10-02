c4281cb5d5ab2b1f9b0852ede4a2e0f49d628414
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 11:26:04 +0530

    Update output.tf

--- modules/lambda/output.tf
+++ modules/lambda/output.tf
@@ -1,7 +1,9 @@
 output "arn" {
-  value = aws_lambda_function.this.arn
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.this.arn
 }
 
 output "function_name" {
-  value = aws_lambda_function.this.function_name
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.this.function_name
 }
