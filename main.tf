c0221dacb48098b2810ceebf418a61a842167d08
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:40:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
+# Root main.tf
 
-provider "aws" {
-  region = "eu-north-1"
-}
-
+# S3 bucket module
 module "s3_bucket" {
   source               = "./modules/s3"
   bucket_name          = "upload-bucket-data-pipeline-8464"
@@ -19,43 +8,41 @@ module "s3_bucket" {
   lambda_function_name = module.lambda_function.function_name
 }
 
+# DynamoDB table module
 module "dynamodb_table" {
   source = "./modules/dynamodb"
-  table_name = "dynamodb-table"
 }
 
-module "lambda_function" {
-  source = "./modules/lambda"
-  lambda_name        = "dynamodb_to_s3"
-  handler_trigger     = "dynamodb_to_s3_trigger.lambda_handler"
-  s3_bucket   = module.s3_bucket.bucket_name
-  table_name  = module.dynamodb_table.table_name
-  dynamodb_stream_arn = module.dynamodb_table.stream_arn
-}
-
-module "glue_catalog" {
-  source         = "./modules/glue/glue_raw"
-  database_name  = "catalog_db"
-  table_name     = "catalog_json_table"
-  s3_location    = "s3://${module.s3_bucket.bucket_name}/data/"
-}
-
-module "databrew" {
-  source       = "./modules/databrew"
-  glue_table   = module.glue_catalog.table_name
-  glue_db      = module.glue_catalog.database_name
-  s3_bucket_name    = module.s3_bucket.bucket_name
-  data_zip     = module.lambda_function.data_zip
-}
-
-module "glue_catalog_cleaned" {
-  source = "./modules/glue/glue_cleaned"
-  database_name = "catalog_cleaned_db"
-  table_name    = "catalog_cleaned_json_table"
-  s3_location   = "s3://${module.s3_bucket.bucket_name}/cleaned/"
-}
-
+# Athena module
 module "athena" {
   source = "./modules/athena"
-  result_output_location = "s3://${module.s3_bucket.bucket_name}/athena-results/"
+}
+
+# Glue raw catalog
+module "glue_catalog" {
+  source = "./modules/glue/glue_raw"
+}
+
+# Glue cleaned catalog
+module "glue_catalog_cleaned" {
+  source = "./modules/glue/glue_cleaned"
+}
+
+# Databrew
+module "databrew" {
+  source = "./modules/databrew"
+}
+
+# Lambda function module
+module "lambda_function" {
+  source = "./modules/lambda"
+
+  # 👇 pass required inputs for your Lambda here
+  function_name = "my-lambda-fn"
+  handler       = "index.handler"
+  runtime       = "python3.12"
+  role_arn      = "arn:aws:iam::123456789012:role/my-lambda-role"
+
+  # Example: if using deployment package
+  filename      = "lambda_function_payload.zip"
 }
