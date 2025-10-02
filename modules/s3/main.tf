497fa7ea415a5bc0fbb84ab65a843a0ddae12c26
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:45:47 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,3 +1,4 @@
+# Create S3 bucket
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   acl    = "private"
@@ -8,11 +9,23 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+# Permission so S3 can invoke Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.this.arn
+}
+
+# S3 bucket notification to Lambda
 resource "aws_s3_bucket_notification" "lambda_trigger" {
   bucket = aws_s3_bucket.this.id
 
   lambda_function {
     lambda_function_arn = var.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
+    events              = ["s3:ObjectCreated:*"]  # Trigger on uploads
   }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
