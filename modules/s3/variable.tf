7b83162f8f8d991943b237b79305eb469943c177
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 11:05:15 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,11 +1,3 @@
-variable "bucket_name" {
-  type = string
-}
-
-variable "lambda_function_arn" {
-  type = string
-}
-
-variable "lambda_function_name" {
-  type = string
-}
+variable "bucket_name" {}
+variable "lambda_function_arn" {}
+variable "lambda_function_name" {}
