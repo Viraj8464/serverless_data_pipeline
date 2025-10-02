523e86816f909616349a91ccd253909a9f3818a2
Viraj8464 <virajpatel8464@gmail.com>
2025-10-02 10:08:16 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,9 +1,4 @@
 variable "bucket_name" {
-  description = "upload-bucket-data-pipeline-8464"
   type        = string
-}
-
-variable "lambda_function_arn" {
-  description = "The ARN of the Lambda function to trigger on S3 events"
-  type        = string
+  description = "The name of the S3 bucket"
 }
