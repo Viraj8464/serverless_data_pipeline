8b2f52737d175c7eba40820ce3a97c6267b4dd5d
Viraj8464 <virajpatel8464@gmail.com>
2025-10-04 17:32:24 +0530

    Update output.tf

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,9 +1,9 @@
-output "s3_bucket_name" {
-  description = "The name of the created S3 bucket"
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
   value       = aws_s3_bucket.this.bucket
 }
 
-output "sample_object_url" {
-  description = "The S3 URL of the uploaded sample object"
-  value       = "s3://${aws_s3_bucket.this.bucket}/${aws_s3_object.sample_data.key}"
+output "bucket_arn" {
+  description = "ARN of the created S3 bucket"
+  value       = aws_s3_bucket.this.arn
 }
