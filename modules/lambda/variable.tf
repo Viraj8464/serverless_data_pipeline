8af9a32127b7cdcfe8174fe10acc6f77edec9b7c
Viraj8464 <virajpatel8464@gmail.com>
2025-10-04 17:30:22 +0530

    Update variable.tf

--- modules/lambda/variable.tf
+++ modules/lambda/variable.tf
@@ -1,5 +1,24 @@
-variable "function_name" {}
-variable "handler" {}
-variable "runtime" {}
-variable "role_arn" {}
-variable "filename" {}
+variable "function_name" {
+  type        = string
+  description = "Name of the Lambda function"
+}
+
+variable "handler" {
+  type        = string
+  description = "Lambda handler"
+}
+
+variable "runtime" {
+  type        = string
+  description = "Lambda runtime"
+}
+
+variable "role_arn" {
+  type        = string
+  description = "IAM Role ARN for Lambda"
+}
+
+variable "filename" {
+  type        = string
+  description = "Lambda deployment package zip"
+}
