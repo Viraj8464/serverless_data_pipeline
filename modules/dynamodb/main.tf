3271560d1a0a91c7847d79d9bfc3c1903ad54598
Viraj8464 <virajpatel8464@gmail.com>
2025-10-07 03:45:15 +0530

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,13 +1,10 @@
 resource "aws_dynamodb_table" "this" {
-  name           = var.table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  name         = "pipeline_metadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
     type = "S"
   }
-
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
 }
