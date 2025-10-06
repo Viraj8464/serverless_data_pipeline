ec98b0e27f8a2bf6b9f968604c97da40db9faafb
Viraj8464 <virajpatel8464@gmail.com>
2025-10-07 03:44:09 +0530

    Update variable.tf

--- modules/glue/glue_raw/variable.tf
+++ modules/glue/glue_raw/variable.tf
@@ -1,16 +1,4 @@
 variable "bucket_name" {
-  description = "The S3 bucket name to use for data or query results"
   type        = string
-}
-variable "database_name" {
-  description = "Glue Catalog Database Name"
-  type        = string
-}
-variable "table_name" {
-  description = "Glue Catalog Table Name"
-  type        = string
-}
-variable "s3_location" {
-  description = "S3 Bucket Path"
-  type        = string
+  description = "S3 bucket name for Glue data"
 }
