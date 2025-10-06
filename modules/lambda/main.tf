9836a4e621a1d535e7122be17c0ba9a90ee53baa
Viraj8464 <virajpatel8464@gmail.com>
2025-10-07 03:39:57 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,8 +1,29 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "this" {
-  function_name    = var.function_name
-  handler          = var.handler
-  runtime          = var.runtime
-  role             = var.role_arn
-  filename         = var.filename
-  source_code_hash = filebase64sha256(var.filename)
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip.output_path
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.this.arn
 }
