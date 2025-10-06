e925e09d015163939dd2664d218c1b39de3b02eb
Viraj8464 <virajpatel8464@gmail.com>
2025-10-07 03:42:06 +0530

    Update main.tf

--- modules/athena/main.tf
+++ modules/athena/main.tf
@@ -1,9 +1,8 @@
 resource "aws_athena_workgroup" "this" {
-  name = "my-athena-workgroup"
+  name = "athena_workgroup"
   configuration {
     result_configuration {
-     output_location = "s3://${var.bucket_name}/athena-results/"
+      output_location = "s3://${var.bucket_name}/athena-results/"
     }
   }
-  force_destroy = true
 }
