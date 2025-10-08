932828fe116517ff1f7fee0396405efa227f6986
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:44:52 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,29 +1,41 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.function_name}_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/src"
-  output_path = "${path.module}/lambda.zip"
-}
-
 resource "aws_lambda_function" "this" {
-  function_name = var.function_name
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "handler.lambda_handler"
+  function_name = var.lambda_function_name
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.iam_role_name}"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
-  filename      = data.archive_file.lambda_zip.output_path
+  filename      = "src/lambda_payload.zip"
+
+  environment {
+    variables = {
+      BUCKET_NAME = var.bucket_name
+    }
+  }
 }
 
-output "lambda_function_arn" {
+data "aws_caller_identity" "current" {}
+
+resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
+  event_source_arn  = var.dynamodb_stream_arn
+  function_name     = aws_lambda_function.this.arn
+  starting_position = "LATEST"
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.bucket_name}"
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.this.function_name
+}
+
+output "lambda_arn" {
   value = aws_lambda_function.this.arn
+}
+
+output "lambda_permission" {
+  value = aws_lambda_permission.allow_s3.id
 }
