7e565cc9619f8ea6cfae05f3a195646c5fbe5735
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:44:05 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,9 +1,3 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket to create"
-  type        = string
-}
-
-variable "lambda_function_arn" {
-  description = "Lambda function ARN for S3 trigger"
-  type        = string
-}
+variable "bucket_name" {}
+variable "lambda_function_arn" { default = "" }
+variable "lambda_permission" { default = "" }
