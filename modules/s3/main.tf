a175938e1375c755888641f76deb0e7d773cb155
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:43:32 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,19 +2,18 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_object" "sample_data" {
-  bucket = aws_s3_bucket.this.bucket
-  key    = "sample-data/example.txt"
-  content = "This is sample data"
-}
-
 resource "aws_s3_bucket_notification" "lambda_trigger" {
   bucket = aws_s3_bucket.this.id
 
   lambda_function {
     lambda_function_arn = var.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "data/"
   }
 
-  depends_on = [aws_s3_bucket.this]
+  depends_on = [var.lambda_permission]
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
 }
