f17b0b2904568898e71fc9dfa5719af1bdc82442
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:44:27 +0530

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "this" {
-  name         = "pipeline_metadata"
+  name         = "pipeline-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -7,4 +7,11 @@ resource "aws_dynamodb_table" "this" {
     name = "id"
     type = "S"
   }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+}
+
+output "stream_arn" {
+  value = aws_dynamodb_table.this.stream_arn
 }
