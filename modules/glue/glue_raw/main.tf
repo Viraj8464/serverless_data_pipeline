0a98898ef794ef08ee049994a27861260febd6be
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:47:32 +0530

    Update main.tf

--- modules/glue/glue_raw/main.tf
+++ modules/glue/glue_raw/main.tf
@@ -1,14 +1,24 @@
 resource "aws_glue_catalog_database" "this" {
-  name = "raw_database"
+  name = "cleaned_database"
 }
 
-resource "aws_glue_catalog_table" "this" {
-  name          = "raw_table"
+resource "aws_glue_catalog_table" "cleaned_table" {
+  name          = "cleaned_table"
   database_name = aws_glue_catalog_database.this.name
   table_type    = "EXTERNAL_TABLE"
+
   storage_descriptor {
-    location = "s3://${var.bucket_name}/data/raw/"
+    location      = "s3://${var.bucket_name}/data/cleaned/"
     input_format  = "org.apache.hadoop.mapred.TextInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "OpenCSVSerde"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+    }
   }
+}
+
+output "database_name" {
+  value = aws_glue_catalog_database.this.name
 }
