e24ed8c104ec2034c5a2c830ec827e1fd8f8b31e
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:42:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,31 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 1.0.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "myfirstbucket-8464"
-    key    = "terraform/state/serverless_data_pipeline.tfstate"
-    region = "us-east-1"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Lambda module
-module "lambda_function" {
-  source = "./modules/lambda"
-  function_name = "mylambdafunction"
-}
-
-# S3 bucket module (depends on lambda)
 module "s3_bucket" {
-  source              = "./modules/s3"
-  bucket_name         = "upload-bucket-data-pipeline-8464"
-  lambda_function_arn = module.lambda_function.lambda_function_arn
+  source      = "./modules/s3"
+  bucket_name = var.bucket_name
 }
 
-# Glue Raw Catalog
-module "glue_catalog" {
-  source      = "./modules/glue/glue_raw"
-  bucket_name = module.s3_bucket.s3_bucket_name
+module "dynamodb" {
+  source = "./modules/dynamodb"
 }
 
-# Glue Cleaned Catalog
-module "glue_catalog_cleaned" {
-  source      = "./modules/glue/glue_cleaned"
-  bucket_name = module.s3_bucket.s3_bucket_name
+module "lambda" {
+  source                = "./modules/lambda"
+  bucket_name           = module.s3_bucket.bucket_name
+  lambda_function_name  = var.lambda_function_name
+  iam_role_name         = var.iam_role_name
+  dynamodb_stream_arn   = module.dynamodb.stream_arn
 }
 
-# Athena
-module "athena" {
-  source      = "./modules/athena"
-  bucket_name = module.s3_bucket.s3_bucket_name
-}
-
-# DataBrew
 module "databrew" {
   source      = "./modules/databrew"
-  bucket_name = module.s3_bucket.s3_bucket_name
+  bucket_name = module.s3_bucket.bucket_name
 }
 
-# DynamoDB
-module "dynamodb_table" {
-  source = "./modules/dynamodb"
+module "glue" {
+  source      = "./modules/glue"
+  bucket_name = module.s3_bucket.bucket_name
+}
+
+module "athena" {
+  source      = "./modules/athena"
+  bucket_name = module.s3_bucket.bucket_name
 }
