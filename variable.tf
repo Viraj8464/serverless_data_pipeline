dcf5dbb7ee5ab61814946ea5a34d861fcd256b45
Viraj8464 <virajpatel8464@gmail.com>
2025-10-08 14:41:27 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "bucket_name" {
+  default = "upload-bucket-data-pipeline-8464"
+}
+
+variable "lambda_function_name" {
+  default = "mylambdafunction"
+}
+
+variable "iam_role_name" {
+  default = "myrole"
+}
