d1d9ba1352885e08348e9becfec55c5e193f6e92
Viraj8464 <virajpatel8464@gmail.com>
2025-10-09 20:34:37 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,20 +1,11 @@
-output "lambda_function_arn" {
-  description = "Lambda function ARN from module"
-  value       = module.lambda_function.lambda_function_arn
-}
-
 output "s3_bucket_name" {
-  value = module.s3_bucket.bucket_name
+  value = module.s3.bucket_name
 }
 
 output "lambda_function_name" {
-  value = module.lambda.lambda_name
+  value = module.lambda.function_name
 }
 
-output "glue_database" {
-  value = module.glue.database_name
-}
-
-output "athena_workgroup" {
-  value = module.athena.workgroup_name
+output "lambda_role_name" {
+  value = aws_iam_role.lambda_role.name
 }
