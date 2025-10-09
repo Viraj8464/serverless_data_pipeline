43e6281c1b1cd9b47f2e3e6aff3b01a51d9cc327
Viraj8464 <virajpatel8464@gmail.com>
2025-10-09 20:51:39 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,3 +1,16 @@
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = aws_s3_bucket.this.id
+
+  lambda_function {
+    lambda_function_arn = var.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "data/raw/"
+  }
+
+  depends_on = [aws_s3_bucket.this]
+}
+
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
