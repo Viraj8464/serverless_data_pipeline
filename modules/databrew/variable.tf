bff911c191e172e5d3b80663d6e1b669d4400e7f
Viraj8464 <virajpatel8464@gmail.com>
2025-10-09 20:41:44 +0530

    Update variable.tf

--- modules/databrew/variable.tf
+++ modules/databrew/variable.tf
@@ -1,2 +1,9 @@
-variable "bucket_name" {}
-variable "iam_role_name" {}
+variable "bucket_name" {
+  description = "S3 bucket used by DataBrew"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "IAM role ARN for DataBrew"
+  type        = string
+}
