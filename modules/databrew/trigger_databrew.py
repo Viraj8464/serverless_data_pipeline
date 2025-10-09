96e9af7b5d6f1c4058647fe44cc9ac562499d149
Viraj8464 <virajpatel8464@gmail.com>
2025-10-09 20:55:55 +0530

    Update trigger_databrew.py

--- modules/databrew/trigger_databrew.py
+++ modules/databrew/trigger_databrew.py
@@ -1,9 +1,17 @@
 import boto3
+import json
 import os
 
 def lambda_handler(event, context):
+    print("Event received:", json.dumps(event))
+    
     client = boto3.client('databrew')
-    job_name = os.environ['JOB_NAME']
-    response = client.start_job_run(Name=job_name)
-    print(f"Triggered DataBrew job: {job_name}")
-    return response
+    job_name = "databrew-cleaning-project"  # same as in Terraform
+
+    try:
+        response = client.start_job_run(Name=job_name)
+        print("Started DataBrew job:", response['RunId'])
+    except Exception as e:
+        print("Error starting job:", str(e))
+
+    return {"status": "DataBrew job triggered"}
