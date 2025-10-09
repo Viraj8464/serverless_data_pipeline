b01caad3b65cff8fe2f7bf8362f886f17b13a160
Viraj8464 <virajpatel8464@gmail.com>
2025-10-09 20:55:25 +0530

    Update output.tf

--- modules/lambda/output.tf
+++ modules/lambda/output.tf
@@ -5,3 +5,12 @@ output "lambda_function_arn" {
 output "function_name" {
   value = aws_lambda_function.this.function_name
 }
+output "lambda_functions" {
+  value = {
+    databrew_trigger        = aws_lambda_function.databrew_trigger.function_name
+    databrew_trigger_arn    = aws_lambda_function.databrew_trigger.arn
+    dynamodb_to_s3          = aws_lambda_function.dynamodb_to_s3.function_name
+    dynamodb_to_s3_arn      = aws_lambda_function.dynamodb_to_s3.arn
+  }
+}
+
