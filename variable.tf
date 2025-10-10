a885fa75c1c7923cd2af8722b7defa429593d891
Viraj8464 <virajpatel8464@gmail.com>
2025-10-10 15:47:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,12 +12,9 @@ variable "role_name" {
   description = "IAM role name for Lambda"
   default     = "myrole"
 }
-variable "table_name" {
+variable "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
   type        = string
   default     = "my-dynamodb-table"
 }
-module "dynamodb" {
-  source     = "./modules/dynamodb"
-  table_name = var.table_name
-}
+
