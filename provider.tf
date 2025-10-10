040cdc83b25ce832caaad6c2b5cc1be5a9244f89
Viraj8464 <virajpatel8464@gmail.com>
2025-10-10 16:05:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 0.27"   # latest version
     }
   }
-  
 }
+
 
 provider "aws" {
   region = var.region
