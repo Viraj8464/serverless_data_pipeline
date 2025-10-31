7e917d023c8a0deeeeacacbbca8d909fa823d502
Viraj8464 <virajpatel8464@gmail.com>
2025-10-31 14:30:17 +0530

    Update variable.tf

--- modules/databrew/variable.tf
+++ modules/databrew/variable.tf
@@ -1,9 +1,37 @@
 variable "bucket_name" {
-  description = "S3 bucket used by DataBrew"
+  description = "bucket name for store data"
   type        = string
 }
 
-variable "role_arn" {
-  description = "IAM role ARN for DataBrew"
+variable "dataset_name" {
   type        = string
+  description = "Dataset name for DataBrew"
+  default     = "rawdataset"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name for DataBrew"
+  default     = "cleanproject"
+}
+
+
+variable "iam_role_arn" {
+  description = "The ARN of the IAM role for DataBrew job execution"
+  type        = string
+}
+
+variable "recipe_name" {
+  description = "The name of the DataBrew recipe"
+  type        = string
+}
+
+variable "iam_role_name" {
+  description = "IAM role name for AWS DataBrew"
+  type        = string
+}
+variable "job_name" {
+  description = "DataBrew job name"
+  type        = string
+  default     = "cleanjob"
 }
