0f01713c5eac4a87e2f32a6512211c9a356c4e21
Viraj8464 <virajpatel8464@gmail.com>
2025-10-31 14:33:58 +0530

    Update main.tf

--- modules/glue/glue_cleaned/main.tf
+++ modules/glue/glue_cleaned/main.tf
@@ -1,11 +1,25 @@
 resource "aws_glue_catalog_database" "this" {
-  name = "glue_database"
+  name = "cleaned_database"
 }
 
-# example usage of bucket_name
-resource "aws_glue_crawler" "this" {
+resource "aws_glue_catalog_table" "cleaned_table" {
+  name          = "cleaned_table"
   database_name = aws_glue_catalog_database.this.name
-  s3_target {
-    path = "s3://${var.bucket_name}/data/"
+  table_type    = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    location      = "s3://${var.bucket_name}/data/cleaned/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "OpenCSVSerde"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+    }
   }
 }
+
+output "database_name" {
+  value = aws_glue_catalog_database.this.name
+}
+
