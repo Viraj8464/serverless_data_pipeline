5e1bd36f1e0997c96d09fa5f6cb4c15687998f93
Viraj8464 <virajpatel8464@gmail.com>
2025-10-31 14:28:29 +0530

    Update main.tf

--- modules/databrew/main.tf
+++ modules/databrew/main.tf
@@ -1,66 +1,29 @@
-resource "aws_databrew_dataset" "raw_dataset" {
-  name        = "raw-dataset"
-  input {
-    s3_input_definition {
-      bucket = var.bucket_name
-      key    = "data/"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
     }
   }
 }
 
-resource "aws_databrew_recipe" "clean_recipe" {
-  name   = "cleaning-recipe"
-  steps_file_content = file("${path.module}/databrew_recipe.yaml")
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_databrew_project" "clean_project" {
-  name         = "databrew-clean-project"
-  dataset_name = aws_databrew_dataset.raw_dataset.name
-  recipe_name  = aws_databrew_recipe.clean_recipe.name
-  role_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.iam_role_name}"
-}
+# Deploy DataBrew resources using CloudFormation
+resource "aws_cloudformation_stack" "databrew_stack" {
+  name          = "databrew-stack"
+  template_body = file("${path.module}/databrew_cf.yaml")
 
-resource "aws_databrew_job" "clean_job" {
-  name           = "databrew-clean-job"
-  project_name   = aws_databrew_project.clean_project.name
-  type           = "RECIPE"
-  role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.iam_role_name}"
-
-  outputs {
-    location {
-      bucket = var.bucket_name
-      key    = "data/cleaned/"
-    }
-    format = "CSV"
+parameters = {
+    BucketName  = var.bucket_name
+    RoleArn     = var.iam_role_arn
+    RecipeName  = "cleanrecipe1"
+    DatasetName = var.dataset_name
+    ProjectName = var.project_name
+    JobName     = var.job_name
   }
-}
 
-# Automatically trigger DataBrew job when S3 gets new objects in /data
-resource "aws_s3_bucket_notification" "databrew_trigger" {
-  bucket = var.bucket_name
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.databrew_trigger.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "data/"
-  }
-}
-
-resource "aws_lambda_function" "databrew_trigger" {
-  function_name = "databrewTrigger"
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.iam_role_name}"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-
-  filename = "${path.module}/trigger_databrew.zip"
-
-  environment {
-    variables = {
-      JOB_NAME = aws_databrew_job.clean_job.name
-    }
-  }
-}
-
-output "databrew_job_name" {
-  value = aws_databrew_job.clean_job.name
+  capabilities = ["CAPABILITY_IAM"]
 }
