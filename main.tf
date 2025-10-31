d181178b1927b98114ad5e59ef7e7b2ece3576d5
Viraj8464 <virajpatel8464@gmail.com>
2025-10-31 15:26:07 +0530

    Rename IAM role from 'myrole' to 'newrole'

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "awscc" {
 # 1️⃣ IAM Role for Lambda
 # -----------------------
 resource "aws_iam_role" "lambda_role" {
-  name = "myrole"
+  name = "newrole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
