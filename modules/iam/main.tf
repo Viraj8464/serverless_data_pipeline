04d1a8f2422c1c4ce17daaf0084adae501f49055
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:43:43 +0530

    Update IAM roles and policies for Databrew and Lambda

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,26 +1,18 @@
 resource "aws_iam_role" "databrew_role" {
-  name = "newrole"
+  name = "myrole-databrew"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = [
-            "lambda.amazonaws.com",
-            "glue.amazonaws.com",
-            "databrew.amazonaws.com"
-          ]
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "databrew.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_policy" "databrew_s3_access" {
-  name   = "databrew-s3-access"
+resource "aws_iam_role_policy" "databrew_policy" {
+  role = aws_iam_role.databrew_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -28,13 +20,31 @@ resource "aws_iam_policy" "databrew_s3_access" {
       {
         Effect   = "Allow",
         Action   = ["s3:GetObject","s3:PutObject"],
-        Resource = "arn:aws:s3:::${var.bucket_name}/data/*"
+        Resource = "arn:aws:s3:::${var.bucket_name}/*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = "s3:ListBucket",
+        Resource = "arn:aws:s3:::${var.bucket_name}"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "databrew_s3_attach" {
-  role       = aws_iam_role.databrew_role.name
-  policy_arn = aws_iam_policy.databrew_s3_access.arn
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-databrew-trigger"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
