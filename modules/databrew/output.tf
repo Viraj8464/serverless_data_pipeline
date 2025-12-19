f23917d60719af07d105fc4fc98027a7d37dddf0
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:48:49 +0530

    Update output.tf

--- modules/databrew/output.tf
+++ modules/databrew/output.tf
@@ -1,11 +1,3 @@
-# Output the CloudFormation stack name and outputs
-
-output "databrew_stack_name" {
-  description = "Name of the deployed DataBrew CloudFormation stack"
-  value       = aws_cloudformation_stack.databrew_stack.name
-}
-
-output "databrew_stack_outputs" {
-  description = "Outputs from the DataBrew CloudFormation stack"
-  value       = aws_cloudformation_stack.databrew_stack.outputs
+output "stack_name" {
+  value = aws_cloudformation_stack.databrew_stack.name
 }
