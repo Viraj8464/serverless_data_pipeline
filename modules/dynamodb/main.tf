8a75b6367165d70745618c8dcb4ed0dfb1dc99a5
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:52:56 +0530

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,17 +1,1 @@
-resource "aws_dynamodb_table" "this" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
 
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  stream_enabled   = true
-  stream_view_type = "NEW_IMAGE"
-}
-
-output "stream_arn" {
-  value = aws_dynamodb_table.this.stream_arn
-}
