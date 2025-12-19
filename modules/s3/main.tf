c621722ca64e634a8a7d2ca4d629d5af3f91b445
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:45:17 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,28 +1,21 @@
-resource "aws_s3_bucket_notification" "lambda_trigger" {
-  bucket = aws_s3_bucket.this.id
-
-  lambda_function {
-    lambda_function_arn = var.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "data/raw/"
-  }
-
-  depends_on = [aws_s3_bucket.this]
-}
-
-
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.this.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.this.arn
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
+resource "aws_s3_bucket_notification" "notify" {
+  bucket = aws_s3_bucket.this.id
+
+  lambda_function {
+    lambda_function_arn = var.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "data/"
+  }
 }
