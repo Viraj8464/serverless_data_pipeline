d148854a731c5ab66e2ff038b4887360c8bcbcfe
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:53:24 +0530

    Update variable.tf

--- modules/dynamodb/variable.tf
+++ modules/dynamodb/variable.tf
@@ -1,8 +1,1 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-variable "table_name" {
-  description = "DynamoDB Table Name"
-  type        = string
-}
+
