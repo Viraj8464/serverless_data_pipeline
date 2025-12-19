0c5077885115a656da178cca97d37a9c9395eda9
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:45:43 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,9 +1,7 @@
-variable "lambda_function_arn" {
-  description = "ARN of Lambda function to trigger on S3 upload"
-  type        = string
+variable "bucket_name" {
+  type = string
 }
 
-variable "bucket_name" {
-  description = "S3 bucket name"
-  type        = string
+variable "lambda_arn" {
+  type = string
 }
