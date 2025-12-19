f7cb24b17030c92484194b177242647aac563891
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:47:15 +0530

    Update variable.tf

--- modules/lambda/variable.tf
+++ modules/lambda/variable.tf
@@ -1,25 +1,7 @@
-variable "bucket_arn" {
-  description = "ARN of S3 bucket that triggers Lambda"
-  type        = string
-}
-
-variable "function_name" {
-  description = "Lambda function name"
-  type        = string
-}
-
 variable "role_arn" {
-  description = "IAM role ARN for Lambda"
-  type        = string
+  type = string
 }
 
-variable "bucket_name" {
-  description = "S3 bucket used by Lambda"
-  type        = string
+variable "databrew_job" {
+  type = string
 }
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket containing the Lambda code"
-  type        = string
-}
-
-
