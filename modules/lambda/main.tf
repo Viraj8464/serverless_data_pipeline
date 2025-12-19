f35506049a5f14e24dd0ae67d38d01e9a3fb78a2
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:46:53 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,61 +1,9 @@
-# -----------------------
-# IAM Role for Lambda
-# -----------------------
-resource "aws_iam_role" "lambda_role" {
-  name = "newrole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = [
-            "lambda.amazonaws.com",
-            "glue.amazonaws.com",
-            "databrew.amazonaws.com"
-          ]
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
-
-# -----------------------
-# Lambda Function
-# -----------------------
-resource "aws_lambda_function" "mylambda" {
-  function_name = "newlambdafunction"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "src.handler.lambda_handler" # Python file and function
+resource "aws_lambda_function" "this" {
+  function_name = "mylambdafunction"
+  role          = var.role_arn
+  handler       = "start_databrew_job.lambda_handler"
   runtime       = "python3.9"
-  s3_bucket     = var.s3_bucket_name
-  s3_key        = "lambda.zip"
-}
 
-resource "aws_iam_role_policy" "databrew_s3_access" {
-  name = "databrew-s3-access"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket",
-           "s3:DeleteObject"
-        ]
-        Resource = [
-          "arn:aws:s3:::upload-bucket-data-pipeline",
-          "arn:aws:s3:::upload-bucket-data-pipeline/*"
-        ]
-      }
-    ]
-  })
+  filename         = "${path.module}/../../lambda_code/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../../lambda_code/lambda.zip")
 }
