4892a3cd08e70096a80b7f71845b65209fedda4a
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:48:26 +0530

    Clean up DataBrew variable definitions
    Removed unused variables related to DataBrew.

--- modules/databrew/variable.tf
+++ modules/databrew/variable.tf
@@ -1,37 +1,7 @@
 variable "bucket_name" {
-  description = "bucket name for store data"
-  type        = string
+  type = string
 }
 
-variable "dataset_name" {
-  type        = string
-  description = "Dataset name for DataBrew"
-  default     = "rawdataset"
-}
-
-variable "project_name" {
-  type        = string
-  description = "Project name for DataBrew"
-  default     = "cleanproject"
-}
-
-
-variable "iam_role_arn" {
-  description = "The ARN of the IAM role for DataBrew job execution"
-  type        = string
-}
-
-variable "recipe_name" {
-  description = "The name of the DataBrew recipe"
-  type        = string
-}
-
-variable "iam_role_name" {
-  description = "IAM role name for AWS DataBrew"
-  type        = string
-}
-variable "job_name" {
-  description = "DataBrew job name"
-  type        = string
-  default     = "cleanjob"
+variable "role_arn" {
+  type = string
 }
