17b91782fda900142e7f58d09d820cd6191a639c
Viraj8464 <virajpatel8464@gmail.com>
2025-12-19 12:48:01 +0530

    Update main.tf

--- modules/databrew/main.tf
+++ modules/databrew/main.tf
@@ -1,28 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Deploy DataBrew resources using CloudFormation
 resource "aws_cloudformation_stack" "databrew_stack" {
-  name          = "databrewstack"
+  name          = "databrew-stack"
   template_body = file("${path.module}/databrew_cf.yaml")
 
-parameters = {
-    BucketName  = var.bucket_name
-    RoleArn     = var.iam_role_arn
-    RecipeName  = "cleanrecipe1"
-    DatasetName = var.dataset_name
-    ProjectName = var.project_name
-    JobName     = var.job_name
+  parameters = {
+    BucketName = var.bucket_name
+    RoleArn   = var.role_arn
   }
 
   capabilities = ["CAPABILITY_IAM"]
