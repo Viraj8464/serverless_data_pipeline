658458776fc4d2ccc598d00bd5bb6405d23937b6
Viraj8464 <virajpatel8464@gmail.com>
2026-01-28 14:03:06 +0530

    Update main.tf

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -1,7 +1,10 @@
+resource "aws_glue_catalog_database" "this" {
+  name = "pipeline_db"
+}
 resource "aws_glue_crawler" "this" {
   name         = "cleaned-crawler"
-  role         = "AWSGlueServiceRoleDefault"
-  database_name = "cleaned_db"
+  role         =  var.glue_role_arn
+  database_name = aws_glue_catalog_database.this.name
 
   s3_target {
     path = "s3://${var.bucket_name}/cleaned/"
