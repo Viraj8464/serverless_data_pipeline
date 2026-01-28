63013e27ecebc438a5ff4a204a139c9608c63faf
Viraj8464 <virajpatel8464@gmail.com>
2026-01-28 14:07:00 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,6 @@
+############################
+# DataBrew Role
+############################
 resource "aws_iam_role" "databrew_role" {
   name = "myrole-databrew"
 
@@ -5,7 +8,9 @@ resource "aws_iam_role" "databrew_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { Service = "databrew.amazonaws.com" },
+      Principal = {
+        Service = "databrew.amazonaws.com"
+      },
       Action = "sts:AssumeRole"
     }]
   })
@@ -18,19 +23,34 @@ resource "aws_iam_role_policy" "databrew_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["s3:GetObject","s3:PutObject"],
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
         Resource = "arn:aws:s3:::${var.bucket_name}/*"
       },
       {
-        Effect   = "Allow",
-        Action   = "s3:ListBucket",
+        Effect = "Allow",
+        Action = "s3:ListBucket",
         Resource = "arn:aws:s3:::${var.bucket_name}"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
       }
     ]
   })
 }
-
+############################
+# Lambda Role
+############################
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-databrew-trigger"
 
@@ -38,7 +58,9 @@ resource "aws_iam_role" "lambda_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" },
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
       Action = "sts:AssumeRole"
     }]
   })
@@ -47,4 +69,51 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+############################
+# Glue Crawler Role
+############################
+resource "aws_iam_role" "glue_role" {
+  name = "glue-crawler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+# Glue basic service permissions
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+resource "aws_iam_role_policy_attachment" "glue_s3" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# Glue S3 access (CRITICAL)
+resource "aws_iam_role_policy" "glue_s3_policy" {
+  role = aws_iam_role.glue_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      Resource = [
+        "arn:aws:s3:::${var.bucket_name}",
+        "arn:aws:s3:::${var.bucket_name}/*"
+      ]
+    }]
+  })
 }
