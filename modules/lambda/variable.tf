8295bc1856892ab546c590cb4f72bb5f3ff1a9f4
Viraj8464 <virajpatel8464@gmail.com>
2026-01-28 14:20:16 +0530

    Update variable.tf

--- modules/lambda/variable.tf
+++ modules/lambda/variable.tf
@@ -1,7 +1,12 @@
+variable "bucket_name" {}
 variable "role_arn" {
   type = string
 }
 
 variable "databrew_job" {
   type = string
+}
+variable "bucket_arn" {
+  description = "ARN of S3 bucket allowed to trigger Lambda"
+  type        = string
 }
